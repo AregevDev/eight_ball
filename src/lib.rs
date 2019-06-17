c04eda4d0edf83f5314bbfb8bed9a6c85f748fd2
AregevDev <aregevdev@gmail.com>
2019-06-17 12:20:10 +0300

    Add all the possible answers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,31 @@
+static POSSIBLE_ANSWERS: [Answer; 20] = [
+    // affirmative answers
+    Answer::new("It is certain.", AnswerType::Affirmative),
+    Answer::new("It is decidedly so.", AnswerType::Affirmative),
+    Answer::new("Without a doubt.", AnswerType::Affirmative),
+    Answer::new("Yes - definitely.", AnswerType::Affirmative),
+    Answer::new("You may rely on it.", AnswerType::Affirmative),
+    Answer::new("As I see it, yes.", AnswerType::Affirmative),
+    Answer::new("Most likely.", AnswerType::Affirmative),
+    Answer::new("Outlook good.", AnswerType::Affirmative),
+    Answer::new("Yes.", AnswerType::Affirmative),
+    Answer::new("Signs point to yes.", AnswerType::Affirmative),
+
+    // non-committal answers
+    Answer::new("Reply hazy, try again.", AnswerType::NonCommittal),
+    Answer::new("Ask again later.", AnswerType::NonCommittal),
+    Answer::new("Better not tell you now.", AnswerType::NonCommittal),
+    Answer::new("Cannot predict now.", AnswerType::NonCommittal),
+    Answer::new("Concentrate and ask again.", AnswerType::NonCommittal),
+
+    // negative answers
+    Answer::new("Don't count on it.", AnswerType::Negative),
+    Answer::new("My reply is no.", AnswerType::Negative),
+    Answer::new("My sources say no.", AnswerType::Negative),
+    Answer::new("Outlook not so good.", AnswerType::Negative),
+    Answer::new("Very doubtful.", AnswerType::Negative),
+];
+
 #[derive(Debug, Default, Ord, PartialOrd, Eq, PartialEq, Copy, Clone, Hash)]
 pub enum AnswerType {
     Affirmative,
@@ -7,6 +35,12 @@ pub enum AnswerType {
 
 #[derive(Debug, Default, Ord, PartialOrd, Eq, PartialEq, Copy, Clone, Hash)]
 pub struct Answer {
-    content: String,
+    content: &'static str,
     question_type: AnswerType,
+}
+
+impl Answer {
+    pub const fn new(content: &'static str, answer_type: AnswerType) -> Self {
+        Answer { content, question_type }
+    }
 }
