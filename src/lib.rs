d1928f57e5d49cbb9c9b6c4f1f6c2356491f0dfd
AregevDev <aregevdev@gmail.com>
2019-07-18 14:50:18 +0300

    Implement custom answers

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,56 @@
 use rand::seq::SliceRandom;
 
-/// Picks a random answer from the possible answers
-pub fn random_answer() -> Answer {
-    *POSSIBLE_ANSWERS.choose(&mut rand::thread_rng()).unwrap()
+/// The crate's entry point, holdding the current possible answers
+pub struct EightBall {
+    pub answers: Vec<Answer>,
 }
 
-/// Asks the magic ball a question returns a possible answer randomly
-pub fn ask(_question: &str) -> Answer {
-    random_answer()
-}
+impl EightBall {
+    /// constructs a new EightBall, the possibe answers are matching the original answers  
+    pub fn new() -> Self {
+        EightBall {
+            answers: vec![
+                // affirmative answers
+                Answer::new("It is certain.", AnswerType::Affirmative),
+                Answer::new("It is decidedly so.", AnswerType::Affirmative),
+                Answer::new("Without a doubt.", AnswerType::Affirmative),
+                Answer::new("Yes - definitely.", AnswerType::Affirmative),
+                Answer::new("You may rely on it.", AnswerType::Affirmative),
+                Answer::new("As I see it, yes.", AnswerType::Affirmative),
+                Answer::new("Most likely.", AnswerType::Affirmative),
+                Answer::new("Outlook good.", AnswerType::Affirmative),
+                Answer::new("Yes.", AnswerType::Affirmative),
+                Answer::new("Signs point to yes.", AnswerType::Affirmative),
+                // non-committal answers
+                Answer::new("Reply hazy, try again.", AnswerType::NonCommittal),
+                Answer::new("Ask again later.", AnswerType::NonCommittal),
+                Answer::new("Better not tell you now.", AnswerType::NonCommittal),
+                Answer::new("Cannot predict now.", AnswerType::NonCommittal),
+                Answer::new("Concentrate and ask again.", AnswerType::NonCommittal),
+                // negative answers
+                Answer::new("Don't count on it.", AnswerType::Negative),
+                Answer::new("My reply is no.", AnswerType::Negative),
+                Answer::new("My sources say no.", AnswerType::Negative),
+                Answer::new("Outlook not so good.", AnswerType::Negative),
+                Answer::new("Very doubtful.", AnswerType::Negative),
+            ],
+        }
+    }
 
-static POSSIBLE_ANSWERS: [Answer; 20] = [
-    // affirmative answers
-    Answer::new("It is certain.", AnswerType::Affirmative),
-    Answer::new("It is decidedly so.", AnswerType::Affirmative),
-    Answer::new("Without a doubt.", AnswerType::Affirmative),
-    Answer::new("Yes - definitely.", AnswerType::Affirmative),
-    Answer::new("You may rely on it.", AnswerType::Affirmative),
-    Answer::new("As I see it, yes.", AnswerType::Affirmative),
-    Answer::new("Most likely.", AnswerType::Affirmative),
-    Answer::new("Outlook good.", AnswerType::Affirmative),
-    Answer::new("Yes.", AnswerType::Affirmative),
-    Answer::new("Signs point to yes.", AnswerType::Affirmative),
-    // non-committal answers
-    Answer::new("Reply hazy, try again.", AnswerType::NonCommittal),
-    Answer::new("Ask again later.", AnswerType::NonCommittal),
-    Answer::new("Better not tell you now.", AnswerType::NonCommittal),
-    Answer::new("Cannot predict now.", AnswerType::NonCommittal),
-    Answer::new("Concentrate and ask again.", AnswerType::NonCommittal),
-    // negative answers
-    Answer::new("Don't count on it.", AnswerType::Negative),
-    Answer::new("My reply is no.", AnswerType::Negative),
-    Answer::new("My sources say no.", AnswerType::Negative),
-    Answer::new("Outlook not so good.", AnswerType::Negative),
-    Answer::new("Very doubtful.", AnswerType::Negative),
-];
+    /// Sets the possible answers the crate can return, this allows for custom answers
+    pub fn with_answers(answers: Vec<Answer>) -> Self {
+        EightBall { answers }
+    }
+
+    /// Asks the magic ball a question returns a possible answer randomly it picks a random answer from the possible ones
+    pub fn ask(&self, _question: &str) -> Option<Answer> {
+        if self.answers.is_empty() {
+            return None;
+        }
+        
+        Some(*self.answers.choose(&mut rand::thread_rng()).unwrap())
+    }
+}
 
 /// The type of an answer, used to determine it's color in the future.
 ///
@@ -57,26 +73,16 @@ impl Default for AnswerType {
 /// Holds the content str and it's type
 #[derive(Debug, Default, Ord, PartialOrd, Eq, PartialEq, Copy, Clone, Hash)]
 pub struct Answer {
-    content: &'static str,
-    answer_type: AnswerType,
+    pub content: &'static str,
+    pub answer_type: AnswerType,
 }
 
 impl Answer {
-    /// Returns a new Answer instance with a given content and type.
+    /// Constructs a new Answer instance with a given content and type.
     pub const fn new(content: &'static str, answer_type: AnswerType) -> Self {
         Answer {
             content,
             answer_type,
         }
-    }
-
-    /// Returns an Answer's content
-    pub fn content(&self) -> &'static str {
-        &self.content
-    }
-
-    /// Returns an Answer's type
-    pub fn answer_type(&self) -> &AnswerType {
-        &self.answer_type
     }
 }
